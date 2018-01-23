a7299291db983693a654060f4e8f371343480144
chemicstry <chemicstry@gmail.com>
2018-01-23 19:11:41 +0200

    Add favicon

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -3,6 +3,7 @@
 var path = require('path');
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
 module.exports = {
     entry: [
@@ -18,6 +19,39 @@ module.exports = {
             template: 'client/index.tpl.html',
             inject: 'body',
             filename: 'index.html'
+        }),
+        new FaviconsWebpackPlugin({
+            // Your source logo
+            logo: './client/favicon.png',
+            // The prefix for all image files (might be a folder or a name)
+            prefix: 'icons-[hash]/',
+            // Emit all stats of the generated icons
+            emitStats: false,
+            // The name of the json containing all favicon information
+            statsFilename: 'iconstats-[hash].json',
+            // Generate a cache file with control hashes and
+            // don't rebuild the favicons until those hashes change
+            persistentCache: true,
+            // Inject the html into the html-webpack-plugin
+            inject: true,
+            // favicon background color (see https://github.com/haydenbleasel/favicons#usage)
+            background: '#fff',
+            // favicon app title (see https://github.com/haydenbleasel/favicons#usage)
+            title: '#recksplorer',
+        
+            // which icons should be generated (see https://github.com/haydenbleasel/favicons#usage)
+            icons: {
+              android: true,
+              appleIcon: true,
+              appleStartup: true,
+              coast: false,
+              favicons: true,
+              firefox: true,
+              opengraph: false,
+              twitter: false,
+              yandex: false,
+              windows: false
+            }
         }),
         new webpack.optimize.OccurrenceOrderPlugin(),
         new webpack.optimize.UglifyJsPlugin({
